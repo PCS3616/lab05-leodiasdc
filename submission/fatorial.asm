dd4460baf9a5b7cbe0e266c4efe3f6d3f07b4be8
Leonardo Dias do Carmo <leonardo_carmo@usp.br>
2025-02-21 15:36:07 -0300

    submit fatorial assignment

---
+++ submission/fatorial.asm
@@ -0,0 +1,24 @@
+@ /0000  
+SC /A10  
+HM /002  
+
+@ /0A10  
+JP /000  
+LD N  
+JZ /A26  
+LD RES  
+ML N  
+MM RES  
+LD N  
+SB /006  
+MM N  
+JZ /A26  
+JP /A16  
+RS /A10  
+
+@ /0006  
+K /0001  
+
+@ /0100  
+N K /0003  
+RES K /0001  
